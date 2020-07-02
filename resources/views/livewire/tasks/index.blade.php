e83ea92a33d0637a044e476700acf82ab42e5f0f
Ahmad <contact@ahmad.bz>
2020-07-02 18:43:05 +0300

    css improvements

--- resources/views/livewire/tasks/index.blade.php
+++ resources/views/livewire/tasks/index.blade.php
@@ -6,8 +6,8 @@
     
             <livewire:tasks.quick-create :projectid="$projectid" />
  
-            <h4 class="border-b-2 border-gray-500 p-2 mb-5 text-gray-600 font-bold flex items-center ">
-                <svg class="w-4 h-4 text-gray-600" fill="currentColor" viewBox="0 0 20 20"><path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"></path></svg>
+            <h4 class="border-b-2 border-gray-400 p-2 mb-5 text-gray-500 font-bold flex items-center ">
+                <svg class="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20"><path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"></path></svg>
                 <span class="mx-2">مهامي</span>
             </h4>
 
--- resources/views/livewire/tasks/task-row.blade.php
+++ resources/views/livewire/tasks/task-row.blade.php
@@ -15,40 +15,42 @@
     </div>
 
     <div x-show="showDetail" class="fixed inset-0 overflow-hidden ">
-        <div class="absolute inset-0 overflow-hidden" >
-        <section class="absolute inset-y-0 right-0 max-w-full flex shadow-outline-indigo"  @click.away="showDetail = false">
-            <div class="w-screen max-w-md">
-            <div class="h-full flex flex-col bg-white overflow-y-scroll">
-                <header class="p-4 sm:p-4 bg-indigo-50">
-                    <div class="flex items-center justify-between space-x-3">
-                        <h2 class="text font-medium text-gray-700">
-                            تفاصيل المهمة
-                        </h2>
-                        <button @click="showDetail= false" aria-label="Close panel" class="text-gray-400 hover:text-gray-500 focus:outline-none transition ease-in-out duration-150">
-                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                            </svg>
-                        </button>
-                
+        <div class="absolute inset-0 overflow-hidden">
+        <div class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+            <section class="absolute inset-y-0 right-0 max-w-full flex"  @click.away="showDetail = false">
+                <div class="w-screen max-w-md">
+                <div class="h-full flex flex-col bg-white overflow-y-scroll">
+                    <header class="p-4 sm:p-4 bg-indigo-50">
+                        <div class="flex items-center justify-between space-x-3">
+                            <h2 class="text font-medium text-gray-700">
+                                تفاصيل المهمة
+                            </h2>
+                            <button @click="showDetail= false" aria-label="Close panel" class="text-gray-400 hover:text-gray-500 focus:outline-none transition ease-in-out duration-150">
+                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                                </svg>
+                            </button>
+                    
+                        </div>
+                    </header>
+                    <div class="relative flex-1 p-1 ">
+                        <div class="text-gray-700 bg-gray-50 p-3 rounded leading-8 text-sm">
+                            {!! nl2br($task->task) !!}
+                        </div>
+                        <span class="p-1 inline-block px-1 text-sm text-gray-300 flex items-center">
+                            <b class="ml-2 text-gray-700 bg-gray-200 py-0.5 rounded-full inline-flex items-center">
+                                <img class="inline-block h-5 w-5 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                <span class="mx-2 text-sm">{{$task->user->name}}</span>
+                            </b>
+                                
+                            {{$task->created_at->diffForHumans()}} 
+                            <span dir="ltr" class="mx-3 text-xs">{{$task->created_at}}</span>
+                        </span>
                     </div>
-                </header>
-                <div class="relative flex-1 p-1 ">
-                    <div class="text-gray-700 bg-gray-50 p-3 rounded leading-8 text-sm">
-                        {!! nl2br($task->task) !!}
-                    </div>
-                    <span class="p-1 inline-block px-1 text-sm text-gray-300 flex items-center">
-                        <b class="ml-2 text-gray-700 bg-gray-200 py-0.5 rounded-full inline-flex items-center">
-                            <img class="inline-block h-5 w-5 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                            <span class="mx-2 text-sm">{{$task->user->name}}</span>
-                        </b>
-                            
-                        {{$task->created_at->diffForHumans()}} 
-                        <span dir="ltr" class="mx-3 text-xs">{{$task->created_at}}</span>
-                    </span>
                 </div>
-            </div>
-            </div>
-        </section>
+                </div>
+            </section>
         </div>
     </div>
 </div>
