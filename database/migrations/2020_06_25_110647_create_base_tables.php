7f4216dfc6bcde1b2552f704d3a4128fed909b23
Enas <pc@enas-sm.cckpitcc.sa>
2020-07-15 12:51:05 +0300

    project deadline

--- database/migrations/2020_06_25_110647_create_base_tables.php
+++ database/migrations/2020_06_25_110647_create_base_tables.php
@@ -16,36 +16,37 @@ class CreateBaseTables extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable();	// owner
-            $table->foreignId('team_id')->nullable();	
+            $table->foreignId('team_id')->nullable();
             $table->string('name');
             $table->text('details')->nullable();
             $table->string('status', 100)->default('active');
             $table->string('color', 20)->default('teal')->nullable();
             $table->string('image')->nullable();
             $table->json('meta')->nullable();
-            $table->softDeletes('deleted_at', 0);	
+            $table->date('deadline')->nullable();
+            $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
 
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable();	// owner / created by 
-            $table->foreignId('project_id')->nullable();	
-            $table->foreignId('parent_id')->nullable();	
+            $table->foreignId('user_id')->nullable();	// owner / created by
+            $table->foreignId('project_id')->nullable();
+            $table->foreignId('parent_id')->nullable();
             $table->text('task');
             $table->text('details')->nullable();
             $table->string('status', 100)->default('active');
             $table->boolean('done')->default(false);
             $table->json('meta')->nullable();
             $table->timestamp('due_date')->nullable();
-            $table->softDeletes('deleted_at', 0);	
+            $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
 
         Schema::create('project_members', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable();	 
-            $table->foreignId('project_id')->nullable();	
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('project_id')->nullable();
             $table->string('rule', 100)->default('member');
             $table->timestamps();
         });
--- resources/views/livewire/projects/settings.blade.php
+++ resources/views/livewire/projects/settings.blade.php
@@ -4,15 +4,26 @@
         <x-fields.wrapper label="اسم المشروع" for="item.name" :error="$errors->first('item.name')">
             <x-fields.text wire:model.lazy="item.name" id="item.name" rules="required"/>
         </x-fields.wrapper>
- 
+
         <x-fields.wrapper label="حالةالمشروع" for="item.status" :error="$errors->first('item.status')">
             <x-fields.text wire:model.lazy="item.status" id="item.status" rules="required"/>
         </x-fields.wrapper>
 
-        <x-fields.wrapper label="تفاصيل المشروع " for="item.status" :error="$errors->first('item.status')">
+        <x-fields.wrapper label="تفاصيل المشروع " for="item.details" :error="$errors->first('item.details')">
             <x-fields.text wire:model.lazy="item.details" id="item.details" rules="required"/>
         </x-fields.wrapper>
- 
+
+        <x-fields.wrapper label="موعد تسليم المشروع " for="item.deadline" :error="$errors->first('item.deadline')">
+            <x-fields.text wire:model.lazy="item.deadline" id="item.deadline" rules="required" type="date"/>
+        </x-fields.wrapper>
+
+        {{--        <x-fields.wrapper label="شعار المشروع " for="item.image" :error="$errors->first('item.image')">--}}
+        {{--        <form wire:submit.prevent="save">--}}
+        {{--            <input type="file" wire:model="image">--}}
+        {{--            @error('image') <span class="error">{{ $message }}</span> @enderror--}}
+        {{--            <button type="submit">رفع الصوره</button>--}}
+        {{--        </form>--}}
+        {{--        </x-fields.wrapper>--}}
         <div class="text-left mt-2">
             <button type="submit"
                     class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-sm text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
@@ -22,4 +33,14 @@
         </div>
     </form>
 
+
 </x-project-page>
+<script>
+    ('#datepicker').datepicker({
+        dateFormat: 'dd-mm-yy',
+    });
+
+    $('#datepicker').on('change', function (e) {
+    @this.set('taskduedate', e.target.value)
+    });
+</script>
