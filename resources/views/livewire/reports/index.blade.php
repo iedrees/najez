b3940a554344e2f087dd56fc411b8d26e0659d73
Ahmad <contact@ahmad.bz>
2020-07-22 11:25:22 +0300

    no wrap tasks

--- resources/views/livewire/reports/index.blade.php
+++ resources/views/livewire/reports/index.blade.php
@@ -53,13 +53,13 @@
             <table class="min-w-full">
               <thead>
                 <tr class="bg-gray-200 border-b-2 border-white">
-                  <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-600">
+                  <td class="px-6 py-4  text-sm leading-5 text-gray-600">
                     المهام
                   </td>
-                  <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-600">
+                  <td class="px-6 py-4  text-sm leading-5 text-gray-600">
                     المشروع
                   </td>
-                  <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-600">
+                  <td class="px-6 py-4  text-sm leading-5 text-gray-600">
                     الموظف
                   </td>
                 </tr>
@@ -67,14 +67,14 @@
               <tbody>
                 @foreach($tasks as $task)
                 <tr class="bg-white @if (!$loop->last) border-b-2 @endif  border-gray-100">
-                  <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-700">
+                  <td class="px-6 py-4  text-sm leading-5 text-gray-700">
                     <div>{{$task->task}} </div>
                     <span dir="ltr" class="text-xs text-gray-300">{{Carbon\Carbon::parse($task->done_at)->format('d M, Y')}}  </span>
                   </td>
-                  <td class="px-6 w-32 py-4 bg-{{$task->project->color}}-50 border-{{$task->project->color}}-100 @if (!$loop->last) border-b-2 @endif font-bold whitespace-no-wrap text-base leading-5 text-gray-500">
+                  <td class="px-6 w-32 py-4 bg-{{$task->project->color}}-50 border-{{$task->project->color}}-100 @if (!$loop->last) border-b-2 @endif font-bold  text-base leading-5 text-gray-500">
                     {{$task->project->name}}
                   </td>
-                  <td class="px-6 w-56  max-w-md py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                  <td class="px-6 w-56  max-w-md py-4  text-sm leading-5 font-medium text-gray-900">
                     <div class="text-gray-700">{{$task->assignedUser->name}}</div>
                     <span dir="ltr" class="text-gray-400 font-bold">#{{$task->assignedUser->id}}</span>
                   </td>
