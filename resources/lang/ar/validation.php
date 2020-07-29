9ead28c5e9af661a3c23062ce5b7be8d8cdeb1f7
Enas <oooomlk36@gmail.com>
2020-07-29 15:03:54 +0300

    translate messg & delete project

---
+++ resources/lang/ar/validation.php
@@ -0,0 +1,225 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => 'يجب قبول :attribute.',
+    'active_url'           => ':attribute لا يُمثّل رابطًا صحيحًا.',
+    'after'                => 'يجب على :attribute أن يكون تاريخًا لاحقًا للتاريخ :date.',
+    'after_or_equal'       => ':attribute يجب أن يكون تاريخاً لاحقاً أو مطابقاً للتاريخ :date.',
+    'alpha'                => 'يجب أن لا يحتوي :attribute سوى على حروف.',
+    'alpha_dash'           => 'يجب أن لا يحتوي :attribute سوى على حروف، أرقام ومطّات.',
+    'alpha_num'            => 'يجب أن يحتوي :attribute على حروفٍ وأرقامٍ فقط.',
+    'array'                => 'يجب أن يكون :attribute ًمصفوفة.',
+    'before'               => 'يجب على :attribute أن يكون تاريخًا سابقًا للتاريخ :date.',
+    'before_or_equal'      => ':attribute يجب أن يكون تاريخا سابقا أو مطابقا للتاريخ :date.',
+    'between'              => [
+        'numeric' => 'يجب أن تكون قيمة :attribute بين :min و :max.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute بين :min و :max كيلوبايت.',
+        'string'  => 'يجب أن يكون عدد حروف النّص :attribute بين :min و :max.',
+        'array'   => 'يجب أن يحتوي :attribute على عدد من العناصر بين :min و :max.',
+    ],
+    'boolean'              => 'يجب أن تكون قيمة :attribute إما true أو false .',
+    'confirmed'            => 'حقل التأكيد غير مُطابق للحقل :attribute.',
+    'date'                 => ':attribute ليس تاريخًا صحيحًا.',
+    'date_equals'          => 'يجب أن يكون :attribute مطابقاً للتاريخ :date.',
+    'date_format'          => 'لا يتوافق :attribute مع الشكل :format.',
+    'different'            => 'يجب أن يكون الحقلان :attribute و :other مُختلفين.',
+    'digits'               => 'يجب أن يحتوي :attribute على :digits رقمًا/أرقام.',
+    'digits_between'       => 'يجب أن يحتوي :attribute بين :min و :max رقمًا/أرقام .',
+    'dimensions'           => 'الـ :attribute يحتوي على أبعاد صورة غير صالحة.',
+    'distinct'             => 'للحقل :attribute قيمة مُكرّرة.',
+    'email'                => 'عفواً، يجب أن يكون :attribute صحيح البُنية.',
+    'exists'               => 'القيمة المحددة :attribute غير موجودة.',
+    'file'                 => 'الـ :attribute يجب أن يكون ملفا.',
+    'filled'               => ':attribute إجباري.',
+    'gt'                   => [
+        'numeric' => 'يجب أن تكون قيمة :attribute أكبر من :value.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute أكبر من :value كيلوبايت.',
+        'string'  => 'يجب أن يكون طول النّص :attribute أكثر من :value حروفٍ/حرفًا.',
+        'array'   => 'يجب أن يحتوي :attribute على أكثر من :value عناصر/عنصر.',
+    ],
+    'gte'                  => [
+        'numeric' => 'يجب أن تكون قيمة :attribute مساوية أو أكبر من :value.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute على الأقل :value كيلوبايت.',
+        'string'  => 'يجب أن يكون طول النص :attribute على الأقل :value حروفٍ/حرفًا.',
+        'array'   => 'يجب أن يحتوي :attribute على الأقل على :value عُنصرًا/عناصر.',
+    ],
+    'image'                => 'يجب أن يكون :attribute صورةً.',
+    'in'                   => ':attribute غير موجود.',
+    'in_array'             => ':attribute غير موجود في :other.',
+    'integer'              => 'يجب أن يكون :attribute عددًا صحيحًا.',
+    'ip'                   => 'يجب أن يكون :attribute عنوان IP صحيحًا.',
+    'ipv4'                 => 'يجب أن يكون :attribute عنوان IPv4 صحيحًا.',
+    'ipv6'                 => 'يجب أن يكون :attribute عنوان IPv6 صحيحًا.',
+    'json'                 => 'يجب أن يكون :attribute نصآ من نوع JSON.',
+    'lt'                   => [
+        'numeric' => 'يجب أن تكون قيمة :attribute أصغر من :value.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute أصغر من :value كيلوبايت.',
+        'string'  => 'يجب أن يكون طول النّص :attribute أقل من :value حروفٍ/حرفًا.',
+        'array'   => 'يجب أن يحتوي :attribute على أقل من :value عناصر/عنصر.',
+    ],
+    'lte'                  => [
+        'numeric' => 'يجب أن تكون قيمة :attribute مساوية أو أصغر من :value.',
+        'file'    => 'يجب أن لا يتجاوز حجم الملف :attribute :value كيلوبايت.',
+        'string'  => 'يجب أن لا يتجاوز طول النّص :attribute :value حرفٍ/حرفًا.',
+        'array'   => 'يجب أن لا يحتوي :attribute على أكثر من :value عناصر/عنصر.',
+    ],
+    'max'                  => [
+        'numeric' => 'يجب أن تكون قيمة :attribute مساوية أو أصغر من :max.',
+        'file'    => 'يجب أن لا يتجاوز حجم الملف :attribute :max كيلوبايت.',
+        'string'  => 'عفواً، يجب أن لا يتجاوز عدد الاحرف :attribute :max حرف.',
+        'array'   => 'يجب أن لا يحتوي :attribute على أكثر من :max عناصر/عنصر.',
+    ],
+    'mimes'                => 'عفوا، صيغة الصورة ليست من الصيغ التالية : :values.',
+    'mimetypes'            => 'يجب أن يكون ملفًا من نوع : :values.',
+    'min'                  => [
+        'numeric' => 'يجب أن تكون قيمة :attribute مساوية أو أكبر من :min.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute على الأقل :min كيلوبايت.',
+        'string'  => 'عفواً، لايمكن اضافة :attribute اقل من :min خانات',
+        'array'   => 'يجب أن يحتوي :attribute على الأقل على :min عُنصرًا/عناصر.',
+    ],
+    'not_in'               => ':attribute موجود.',
+    'not_regex'            => 'صيغة :attribute غير صحيحة.',
+    'numeric'              => 'يجب على :attribute أن يكون رقمًا.',
+    'present'              => 'يجب تقديم :attribute.',
+    'regex'                => 'صيغة :attribute .غير صحيحة.',
+    'required'             => 'حقل :attribute مطلوب.',
+    'required_if'          => ':attribute مطلوب في حال ما إذا كان :other يساوي :value.',
+    'required_unless'      => ':attribute مطلوب في حال ما لم يكن :other يساوي :values.',
+    'required_with'        => ':attribute مطلوب إذا توفّر :values.',
+    'required_with_all'    => ':attribute مطلوب إذا توفّر :values.',
+    'required_without'     => ':attribute مطلوب إذا لم يتوفّر :values.',
+    'required_without_all' => ':attribute مطلوب إذا لم يتوفّر :values.',
+    'same'                 => 'يجب أن يتطابق :attribute مع :other.',
+    'size'                 => [
+        'numeric' => 'يجب أن تكون قيمة :attribute مساوية لـ :size.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute :size كيلوبايت.',
+        'string'  => 'يجب أن يحتوي النص :attribute على :size حروفٍ/حرفًا بالضبط.',
+        'array'   => 'يجب أن يحتوي :attribute على :size عنصرٍ/عناصر بالضبط.',
+    ],
+    'starts_with'          => 'يجب أن يبدأ :attribute بأحد القيم التالية: :values',
+    'string'               => 'يجب أن يكون :attribute نصًا.',
+    'timezone'             => 'يجب أن يكون :attribute نطاقًا زمنيًا صحيحًا.',
+    'unique'               => 'عفواً، :attribute مضاف مسبقاً.',
+    'uploaded'             => 'فشل في تحميل الـ :attribute.',
+    'url'                  => 'صيغة :attribute غير صحيحة.',
+    'uuid'                 => ':attribute يجب أن يكون بصيغة UUID سليمة.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [
+        'name'                              => 'الاسم',
+        'user.name'                              => 'الاسم',
+        'username'                          => 'اسم المُستخدم',
+        'user.username'                          => 'اسم المُستخدم',
+        'user.email'                          => 'البريد الالكتروني',
+        'email'                             => 'البريد الالكتروني',
+        'first_name'                        => 'الاسم الأول',
+        'last_name'                         => 'اسم العائلة',
+        'user.employee_num'                 => 'الرقم الوظيفي',
+        'password'                          => 'كلمة المرور',
+        'is_scheduled_for_auto_reverse'     => 'عملية عكسية تلقائية',
+        'password_confirmation'             => 'تأكيد كلمة المرور',
+        'city'                              => 'المدينة',
+        'country'                           => 'الدولة',
+        'address'               => 'عنوان السكن',
+        'phone'                 => 'الهاتف',
+        'Role'                  => 'الأدوار',
+        'mobile'                => 'الجوال',
+        'age'                   => 'العمر',
+        'sex'                   => 'الجنس',
+        'gender'                => 'الجنس',
+        'day'                   => 'اليوم',
+        'month'                 => 'الشهر',
+        'year'                  => 'السنة',
+        'hour'                  => 'ساعة',
+        'minute'                => 'دقيقة',
+        'second'                => 'ثانية',
+        'title'                 => 'العنوان',
+        'content'               => 'المُحتوى',
+        'description'           => 'الوصف',
+        'excerpt'               => 'المُلخص',
+        'date'                  => 'التاريخ',
+        'time'                  => 'الوقت',
+        'available'             => 'مُتاح',
+        'size'                  => 'الحجم',
+        'file'                  => 'الملف',
+        'picture'               => 'الصورة',
+        'pictureCategory'       => 'تصنيف الصورة',
+        'bio'                   => 'النبذة التعريفية',
+        'bio_category'          => 'تصنيف النبذة التعريفية',
+        'Email_Category'        => 'تصنيف البريد الالكتروني',
+        'name_category'         => 'تصنيف الاسم',
+        'number'                => 'الرقم',
+        'category'              => 'التصنيف',
+        'category_id'           => 'التصنيف',
+        'sim_code'              => 'رمز الشريحة',
+        'ip'                    => 'الاي بي',
+        'port'                  => 'المنفذ',
+        'user_agent_string'     => 'وكيل المستخدم',
+        'screenshot'            => 'الصورة',
+        'url'                   => 'الرابط',
+        'priority'              => 'الأهمية',
+        'service'               => 'الخدمة',
+        'guard_name'            => 'اسم الحارس',
+        'language'              => 'اللغة',
+        'avatar'                => 'الصورة',
+        'role'                  => 'الدور',
+        'phone_service'         => 'مقدم خدمة الجوال',
+        'auto_reverse_after_seconds'    => 'عملية عكسية بعد',
+        'user'                  => 'مستخدم',
+        'typeProblem'          =>'نوع الحادثة',
+        'type_problem'          =>'نوع الحادثة',
+        'dateTime_problem'          =>'وقت وتاريخ الحادثة',
+        'reason_problem'          =>'أسباب الحادث',
+        'scenario_problem'          =>'سيناريو الحادثة',
+        'longTerm_solution'          =>'حلول طويلةالمدى',
+        'shortTerm_solution'          =>'حلول قصيرة المدى',
+        'levels_problem'          =>'مستوى خطورة الحادثة',
+        'solve'          =>' أغلقت بواسطة',
+        'system'          =>'اسم النظام',
+        'item.name'          =>'اسم المشروع',
+        'name_system' =>'اسم النظام',
+        'Subject'=>'عنوان التقرير',
+        'SubSubject'=>'العنوان الفرعي',
+        'ReportDate'=>'تاريخ التقرير',
+
+
+    ],
+];
