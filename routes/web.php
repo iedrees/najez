e8748507aa15c64c8180aa45a2592c6aa1bff3f6
Ahmad <contact@ahmad.bz>
2020-09-30 18:35:29 +0300

    upgrade livewire to version 2

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,32 @@
 
 // Projects
 Route::middleware('auth')->group(function () {
-    Route::livewire('/', 'home.home')->name('home');
+    Route::get('/', \App\Http\Livewire\Home\Home::class)->name('home');
 
     Route::as('projects.')->prefix('/projects/{id}')->group(function () {
-        Route::livewire('/', 'projects.show')->name('show');
-        Route::livewire('/settings', 'projects.settings')->name('settings');
-        Route::livewire('/members', 'projects.members')->name('members');
+        // Route::livewire('/', 'projects.show')->name('show');
+        // Route::livewire('/settings', 'projects.settings')->name('settings');
+        // Route::livewire('/members', 'projects.members')->name('members');
     });
 
-    Route::livewire('create-project', 'projects.project-create')->name('create-project');
+    // Route::livewire('create-project', 'projects.project-create')->name('create-project');
 });
  
-// Download Reports
-Route::get('reports/view-pdf/{start}/{end}', 'PdfController@viewPDF');
-Route::get('reports/download', 'PdfController@download')->name('reports.download');
-
-// Auth
 Route::middleware('auth')->group(function () {
 
     // profile
-    Route::livewire('/profile', 'users.show')->name('profile');
+    Route::get('/profile', \App\Http\Livewire\Users\Show::class)->name('profile');
 
     // Reports
-    Route::livewire('/reports/index', 'reports.index')->name('reports.index');
+    Route::get('/reports/index',\App\Http\Livewire\Reports\Index::class)->name('reports.index');
 
     // My tasks
-    Route::livewire('/tasks/all', 'tasks.my-tasks')->name('tasks.my-tasks');
+    Route::get('/tasks/all',\App\Http\Livewire\Tasks\MyTasks::class)->name('tasks.my-tasks');
+});
+
+// Auth
+Route::middleware('auth')->namespace('App\Http\Controllers')->group(function () {
+
 
     // Auth
     Route::view('email/verify', 'auth.verify')->middleware('throttle:6,1')->name('verification.notice');
@@ -37,10 +37,18 @@ Route::middleware('auth')->group(function () {
     Route::view('password/confirm', 'auth.passwords.confirm')->name('password.confirm');
 });
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest')->namespace('App\Http\Controllers')->group(function () {
     Route::view('login', 'auth.login')->name('login');
     Route::view('register', 'auth.register')->name('register');
 });
 
-Route::view('password/reset', 'auth.passwords.email')->name('password.request');
-Route::get('password/reset/{token}', 'Auth\PasswordResetController')->name('password.reset');
+Route::namespace('App\Http\Controllers')->group(function () {
+
+    // Download Reports
+    Route::get('reports/view-pdf/{start}/{end}', 'PdfController@viewPDF');
+    Route::get('reports/download', 'PdfController@download')->name('reports.download');
+
+    Route::view('password/reset', 'auth.passwords.email')->name('password.request');
+    Route::get('password/reset/{token}', 'Auth\PasswordResetController')->name('password.reset');
+});
+
